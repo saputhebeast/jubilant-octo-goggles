1cc440e6708e7445b863fdc9c3132cf52a54fac3
Sapumal Wijekoon <sapumalwijekoon4@gmail.com>
2021-07-22 19:29:14 +0530

    New

--- src/admin/admin-dashboard.php
+++ src/admin/admin-dashboard.php
@@ -9,11 +9,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/jubilant-octo-goggles/styles/admin-dashboard.css">
     <title>Admin-Dashboard</title>
 </head>
 <body>
     <a href = "admin-logout.php">Logout</a>
+    <a href = "#">Add admin</a>
+
     <h1>Hello, <?php echo $_SESSION['first_name']." ".$_SESSION['last_name']; ?></h1>
+    
+    <script>
+        history.forward(1);
+    </script>
 </body>
 </html>
 <?php
--- src/admin/admin.php
+++ src/admin/admin.php
@@ -1,3 +1,12 @@
+<?php
+    session_start();
+    
+    if(isset($_SESSION['first_name'])){
+        header('Location: admin-dashboard.php');
+        exit();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
