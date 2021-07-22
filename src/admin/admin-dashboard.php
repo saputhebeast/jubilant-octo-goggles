6c8b7f0766c917c007e03b83ea51c05e109e8a22
Sapumal Wijekoon <sapumalwijekoon4@gmail.com>
2021-07-22 18:33:25 +0530

    New

--- src/admin/admin-dashboard.php
+++ src/admin/admin-dashboard.php
@@ -1,12 +1,25 @@
+<?php
+    session_start();
+
+    if(isset($_SESSION['admin_id']) && isset($_SESSION['first_name']) && isset($_SESSION['last_name'])){
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin-Dashboard</title>
 </head>
 <body>
-    <h1>Hello</h1>
+    <a href = "admin-logout.php">Logout</a>
+    <h1>Hello, <?php echo $_SESSION['first_name']." ".$_SESSION['last_name']; ?></h1>
 </body>
 </html>
+<?php
+    }else{
+        header("Location: admin.php");
+        exit();
+    }
+?>
+
--- src/admin/admin-login.php
+++ src/admin/admin-login.php
@@ -26,8 +26,10 @@
             if (mysqli_num_rows($result) === 1) {
                 $row = mysqli_fetch_assoc($result);
                 if ($row['email'] == $email && $row['password'] == $password) {
-                    $_SESSION['username'] = $row['first_name'];
+                    $_SESSION['first_name'] = $row['first_name'];
+                    $_SESSION['last_name'] = $row['last_name'];
                     $_SESSION['admin_id'] = $row['admin_id'];
+                    $_SESSION['email'] = $row['email'];
                     header('Location: admin-dashboard.php');
                 }else{
                     header('Location: admin.php?error=Incorrect email or password');
