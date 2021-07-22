399ad6e724358c3c93fe6c4ef41906f85bc24065
Sapumal Wijekoon <sapumalwijekoon4@gmail.com>
2021-07-22 17:54:33 +0530

    New

--- src/admin/admin-login.php
+++ src/admin/admin-login.php
@@ -2,33 +2,44 @@
     session_start();
     require_once('../resources/config.php');
 
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    if(isset($_POST['email']) && isset($_POST['password'])){
+        function validate($data){
+            $data = trim($data);
+            $data = stripslashes($data);
+            return $data;
+        }
 
-    if(empty($email)){
-        header('Location: admin.php?error=email is required');
-        exit();
-    }else if(empty($password)){
-        header('Location: admin.php?error=password is required');
-        exit();
-    }else{
+        $email = validate($_POST['email']);
+        $password = validate($_POST['password']);
 
-        $sql = "SELECT * FROM admin WHERE email = '$email' AND password = '$password'";
-        $result = mysqli_query($conn, $sql);
-        
-        if (mysqli_num_rows($result) === 1) {
-            $row = mysqli_fetch_assoc($result);
-            if ($row['email'] == $email && $row['password'] == $password) {
-                $_SESSION['username'] = $row['first_name'];
-                $_SESSION['admin_id'] = $row['admin_id'];
-                header('Location: admin-dashboard.php');
+        if(empty($email)){
+            header('Location: admin.php?error=email is required');
+            exit();
+        }else if(empty($password)){
+            header('Location: admin.php?error=password is required');
+            exit();
+        }else{
+    
+            $sql = "SELECT * FROM admin WHERE email = '$email' AND password = '$password'";
+            $result = mysqli_query($conn, $sql);
+            
+            if (mysqli_num_rows($result) === 1) {
+                $row = mysqli_fetch_assoc($result);
+                if ($row['email'] == $email && $row['password'] == $password) {
+                    $_SESSION['username'] = $row['first_name'];
+                    $_SESSION['admin_id'] = $row['admin_id'];
+                    header('Location: admin-dashboard.php');
+                }else{
+                    header('Location: admin.php?error=Incorrect email or password');
+                    exit();
+                }
             }else{
                 header('Location: admin.php?error=Incorrect email or password');
                 exit();
             }
-        }else{
-            header('Location: admin.php?error=Incorrect email or password');
-            exit();
         }
+    }else{
+        header("Location: admin.php");
     }
+    
 ?>
