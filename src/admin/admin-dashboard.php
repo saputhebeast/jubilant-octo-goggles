84f82ff2723c0fdfff045442066ff7140bfb75f6
Sapumal Wijekoon <sapumalwijekoon4@gmail.com>
2021-07-26 19:58:05 +0530

    New

--- src/admin/admin-dashboard.php
+++ src/admin/admin-dashboard.php
@@ -1,4 +1,5 @@
 <?php
+    require "../resources/config.php";
     session_start();
 
     if(isset($_SESSION['admin_id']) && isset($_SESSION['first_name']) && isset($_SESSION['last_name'])){
@@ -25,6 +26,17 @@
     </div>
     
     <h1>Hello, <?php echo $_SESSION['first_name']." ".$_SESSION['last_name']; ?></h1>
+
+    <?php 
+        $admins = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM admin"));
+        $users = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM customer"));
+        $laptops = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM laptop"));
+    
+        echo "<h1>Overview</h1>";
+        echo "<h2>Number of Admins: {$admins}</h2>";
+        echo "<h2>Number of Users: {$users}</h2>";
+        echo "<h2>Number of Laptops: {$laptops}</h2>";
+    ?>
 
 </body>
 </html>
