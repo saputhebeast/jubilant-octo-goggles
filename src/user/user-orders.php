b168d58670c4892ef7ea5447a774782a12dbe853
Sapumal Wijekoon <sapumalwijekoon4@gmail.com>
2021-08-03 00:45:27 +0530

    New

--- src/user/user-orders.php
+++ src/user/user-orders.php
@@ -12,6 +12,13 @@
     <div class="container">
         <h1><?php echo ucfirst($_SESSION['first_name'])?>'s Orders</h1>
     </div>
+    <div class="container">
+        <div class="alert alert-danger" role="alert">
+            <h4 class="alert-heading">You haven't made any order yet!</h4>
+            <hr>
+            <p class="mb-0">Don't miss out on great deals! Start shopping or log in to view products added.</p>
+        </div>
+    </div>
 <?php
     }else{
         header("Location: user.php");
