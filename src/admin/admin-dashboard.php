2f45efadc7c0e59dfa0b29651b560fc9ea6e11e5
Sapumal Wijekoon <sapumalwijekoon4@gmail.com>
2021-08-06 17:27:38 +0530

    New

--- src/admin/admin-dashboard.php
+++ src/admin/admin-dashboard.php
@@ -11,9 +11,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/admin/admin-dashboard.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Admin-Dashboard</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse">
+            <a class="navbar-brand" href="#">Dashboard</a>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav form-inline my-2 my-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div class="nav">
         <a href="admin-dashboard.php" class="active">Dashboard</a>
         <a href="admin-manage.php">Manage Admin</a>
--- src/admin/admin.php
+++ src/admin/admin.php
@@ -13,26 +13,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/admin/admin.css">
+<!--    <link rel="stylesheet" href="../styles/admin/admin.css">-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Admin Login</title>
 </head>
 <body>
-    
-    <form action="admin-login.php" method="post">
-        <!-- error displaying -->
-        <h1>Admin Login</h1>
-        <div class="container">
-            <?php if (isset($_GET['error'])) { ?>
-            <p class='error'><?php echo $_GET['error'] ?></p>
-            <?php } ?>
-
-            <input type="email" name="email" class="email" placeholder="Email"><br>
-            <input type="password" id="password" name="password" class="password" placeholder="Password"><br>
-            <input type="checkbox" name="show" class="show" onclick="showHidePassword()"><label for="">Show Password</label><br>
-            <input type="submit" value="Submit" class="submit">
+    <style>
+        .heading{
+            margin-top: 50px;
+        }
+        .form-holder{
+            margin-top: 10%;
+        }
+        .submit a:hover{
+            text-decoration: none;
+        }
+        .login{
+            margin-top: 10px;
+        }
+        .login .btn{
+            width: 100%;
+        }
+    </style>
+    <div class="container heading">
+        <h1 class="text-center">Admin Login</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4">
+                <div class="card form-holder">
+                    <div class="card-body">
+                        <div class="col col-12">
+                            <?php if (isset($_GET['error'])) { ?>
+                                <p class="alert alert-danger text-center"><?php echo $_GET['error']; ?></p>
+                            <?php } ?>
+                        </div>
+                        <form action="admin-login.php" method="POST">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+                            </div>
+                            <div class="row">
+                                <div class="col-12 text-left">
+                                    <input type="checkbox" onclick="showHidePassword()"> Show Password
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="login col-12 text-center">
+                                    <input type="submit" class="btn btn-primary" value=" Login " />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-    </form>
-    
+    </div>
+
     <script>
         function showHidePassword(){
             var val = document.getElementById('password');
@@ -43,5 +84,5 @@
             }
         }
     </script>
-</body>
+    </body>
 </html>
