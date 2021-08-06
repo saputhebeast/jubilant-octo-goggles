eb2ff6f926a6682d98be06006c7a6ce605e4a922
Sapumal Wijekoon <sapumalwijekoon4@gmail.com>
2021-08-06 19:40:50 +0530

    New

--- src/admin/admin-dashboard.php
+++ src/admin/admin-dashboard.php
@@ -5,21 +5,118 @@
 ?>
 
         <?php require "header.php"?>
-    <h1>Hello, <?php echo $_SESSION['first_name']." ".$_SESSION['last_name']; ?></h1>
-
-    <?php 
+    <?php
         $admins = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM admin"));
         $users = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM customer"));
         $laptops = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM laptop"));
-    
-        echo "<h1>Overview</h1>";
-        echo "<h2>Number of Admins: {$admins}</h2>";
-        echo "<h2>Number of Users: {$users}</h2>";
-        echo "<h2>Number of Laptops: {$laptops}</h2>";
+        $orders = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM item_order"));
+        $keyboards = 0;
+        $mice = 0;
+        $subscribers = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM newsletter"));
+//        echo "<h1>Overview</h1>";
+//        echo "<h2>Number of Admins: {$admins}</h2>";
+//        echo "<h2>Number of Users: {$users}</h2>";
+//        echo "<h2>Number of Laptops: {$laptops}</h2>";
+            date_default_timezone_set("Asia/Colombo");
+            $date = date("h: i a");
     ?>
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Dashboard</title>
+        </head>
+        <body>
+        <style>
+            .container{
+                margin-top: 30px;
+            }
+        </style>
+            <div class="container">
+                <div class="alert-container alert alert-primary" role="alert">
+                    <h1 class="text-center">Admin Dashboard</h1>
+                </div>
+            <div class="container">
+                <div class="card-deck">
+                    <div class="card h-25 d-inline-block">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Registered Admins Count</h5>
+                            <h4 class="card-text text-danger"><strong><?php echo $admins?></strong></h4>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated: <?php echo $date?></small>
+                        </div>
+                    </div>
+                    <div class="card h-25 d-inline-block">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Registered Users Count</h5>
+                            <h4 class="card-text text-danger"><strong><?php echo $users?></strong></h4>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated: <?php echo $date?></small>
+                        </div>
+                    </div>
+                    <div class="card h-25 d-inline-block">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Orders Count</h5>
+                            <h4 class="card-text text-danger"><strong><?php echo $users?></strong></h4>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated: <?php echo $date?></small>
+                        </div>
+                    </div>
 
-</body>
-</html>
+                    <div class="container">
+                        <div class="card-deck">
+                            <div class="card h-25 d-inline-block">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title">Listed Laptops Count</h5>
+                                    <h4 class="card-text text-danger"><strong><?php echo $laptops?></strong></h4>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Last updated: <?php echo $date?></small>
+                                </div>
+                            </div>
+                            <div class="card h-25 d-inline-block">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title">Listed Keyboards Count</h5>
+                                    <h4 class="card-text text-danger"><strong><?php echo $keyboards?></strong></h4>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Last updated: <?php echo $date?></small>
+                                </div>
+                            </div>
+                            <div class="card h-25 d-inline-block">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title">Listed Mice Count</h5>
+                                    <h4 class="card-text text-danger"><strong><?php echo $mice?></strong></h4>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Last updated: <?php echo $date?></small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="card-deck">
+                            <div class="card h-25 d-inline-block">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title">Newsletter Subscribers</h5>
+                                    <h4 class="card-text text-danger"><strong><?php echo $subscribers?></strong></h4>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Last updated: <?php echo $date?></small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+        </html>
 <?php
     }else{
         header("Location: admin.php");
